093350370b8dba3710c1c61ffefdcc76d016c9bf
john-0236 <john-0236@users.noreply.github.com>
2026-08-25 04:06:24 +0100

    fix: compatibility with Prisma 7, postgresql adapter, and missing demo logins

--- db.js
+++ db.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis;
 
 const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
+  adapter,
   log: process.env.NODE_ENV !== 'production' ? ['error', 'warn'] : ['error'],
 });
 
